11ba48badf8a0dc319c71cf20f6bc443bbb2bb90
Giira <euan.a.marsh@gmail.com>
2024-12-03 14:52:40 +0000

    D3 Part 2

--- Day03.py
+++ Day03.py
@@ -14,3 +14,21 @@ def part_1(data):
 print("======== Part 1 ========")
 print(part_1(data))
 
+
+def part_2(data):
+    data = data.split("don't()")
+    new_data = []
+    for i in range(len(data)):
+        if i == 0:
+            new_data.append(data[i])
+        else:
+            tmp = data[i].split("do()")
+            tmp.pop(0)
+            for item in tmp:
+                new_data.append(item)
+    new_data = "".join(new_data)
+    return part_1(new_data)
+
+
+print("======== Part 2 ========")
+print(part_2(data))
