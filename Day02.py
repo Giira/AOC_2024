36a7049dc497e389f17a92d3f3993a5c35c17999
Giira <euan.a.marsh@gmail.com>
2024-12-03 00:32:45 +0000

    D2 Part 2 Started out trying to convert my nested ifs to a function to reduce mess. Remembered boolean all() and any(). Approach to part one stayed the same, but part two opted for brute force rather than the very messy precision I started off with.

--- Day02.py
+++ Day02.py
@@ -2,39 +2,37 @@ with open("Day02.txt") as f:
     data = [line.strip().split(" ") for line in f.readlines()]
 
 
-def main(data):
+def safe_report(report):
+    # all() returns True if all iterables are True
+    asc = all(0 < int(report[i+1]) - int(report[i]) <= 3 for i in range(len(report) - 1)) 
+    desc = all(-3 <= int(report[i+1]) - int(report[i]) < 0 for i in range(len(report) - 1))
+    if asc or desc:
+        return True
+    else:
+        return False
+
+
+def part_1(data):
     total_safe = 0
     for report in data:
-        safe = True
-        if int(report[0]) < int(report[1]):
-            ascending = True
-        elif int(report[0]) > int(report[1]):
-            ascending = False
-        else:
-            safe = False
-        for i in range(len(report) - 1):
-            if safe:
-                if ascending:
-                    if int(report[i]) < int(report[i+1]):
-                        if 1 <= abs(int(report[i+1]) - int(report[i])) <= 3:
-                            continue
-                        else:
-                            safe = False
-                    else:
-                        safe = False
-                else:
-                    if int(report[i]) > int(report[i+1]):
-                        if 1 <= abs(int(report[i]) - int(report[i+1])) <= 3:
-                            continue
-                        else: safe = False
-                    else:
-                        safe = False
-        if safe:
+        if safe_report(report):
             total_safe += 1
+    return total_safe
 
-    
-    print("======== Part 1 ========")
-    print(total_safe)
-    print("======== Part 2 ========")
-    # print(total_2)
-main(data)
+
+total_safe = part_1(data)
+print("======== Part 1 ========")
+print(total_safe)
+
+
+def part_2(data):
+    total_extra_safe = 0
+    for report in data:
+        if any(safe_report(report[:i] + report[i+1:]) for i in range(len(report) + 1)):
+            total_extra_safe += 1
+    return total_extra_safe
+          
+
+total_2 = part_2(data)
+print("======== Part 2 ========")
+print(total_2)
