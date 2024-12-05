0cf6074bf232640480a82518ad4235946174a773
Giira <euan.a.marsh@gmail.com>
2024-12-05 12:53:25 +0000

    D5 Part 2

--- Day05.py
+++ Day05.py
@@ -6,6 +6,7 @@ with open("Day05.txt") as f:
 
 def part_1(data):
     total = 0
+    incorrect_page_sets = []
     for page_set in data[1]:
         valid = True
         for rule in data[0]:
@@ -14,16 +15,33 @@ def part_1(data):
                     valid = False
         if valid:
             total += int(page_set[int(len(page_set) // 2)])
+        else:
+            incorrect_page_sets.append(page_set)
+    return total, incorrect_page_sets
+
+
+def part_2(rules, page_sets):
+    total = 0
+    for page_set in page_sets:
+        valid = False
+        while not valid:
+            passage = False
+            for rule in rules:
+                if rule[0] in page_set and rule[1] in page_set:
+                    if page_set.index(rule[0]) > page_set.index(rule[1]):
+                        page_set.pop(page_set.index(rule[0]))
+                        page_set = page_set[:page_set.index(rule[1])] + [rule[0]] + page_set[page_set.index(rule[1]):]
+                        passage = True
+                        break
+            if not passage:
+                valid = True
+        total += int(page_set[int(len(page_set) // 2)])
     return total
-
-
-def part_2(data):
-    pass
 
 
 if __name__ == "__main__":
     print("======== Part 1 ========")
-    print(part_1(data))
+    print(part_1(data)[0])
 
     print("======== Part 2 ========")
-    print(part_2(data))
+    print(part_2(data[0], part_1(data)[1]))
