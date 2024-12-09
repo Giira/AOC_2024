ac2cf0dcf5d09176891fa898372bee3d391cf7b9
Giira <euan.a.marsh@gmail.com>
2024-12-09 12:13:22 +0000

    D6 Part 2 ... a little bit late

--- Day06.py
+++ Day06.py
@@ -13,41 +13,80 @@ def find_start(data):
                 return i, j
 
 
+start = find_start(data)
+
+
 def make_move(direction, i, j):
+    move = (i,j)
     data[i][j] = "X"
     if direction == "N":
-        if data[i-1][j] != "." and data[i-1][j] != "X":
+        if data[i-1][j] == "#":
             direction = "E"
         else:
             i -= 1
     elif direction == "E":
-        if data[i][j+1] != "." and data[i][j+1] != "X":
+        if data[i][j+1] == "#":
             direction = "S"
         else:
             j += 1
     elif direction == "S":
-        if data[i+1][j] != "." and data[i+1][j] != "X":
+        if data[i+1][j] == "#":
             direction = "W"
         else:
             i += 1
     elif direction == "W":
-        if data[i][j-1] != "." and data[i][j-1] != "X":
+        if data[i][j-1] == "#":
             direction = "N"
         else:
             j -= 1
-    return direction, i, j
+    move = (move, (i, j))
+    return direction, i, j, move
+
 
 def part_1(data):
-    i, j = find_start(data)
+    locations = [(start, "N")]
+    i, j = start
     direction = "N"
-    while 0 < i < NROWS -1 and 0 < j < NCOLS:
-        direction, i, j = make_move(direction, i, j)
-    count = 1
+    while 0 < i < NROWS - 1 and 0 < j < NCOLS - 1:
+        direction, i, j, move = make_move(direction, i, j)
+        if locations[-1][0] != (i, j):
+            locations.append(((i,j), direction))
+    data[i][j] = "X"
+    locations.append(((i, j), direction))
+    count = 0
     for line in data:
         count += line.count("X")
-    return count
+    return count, locations
     
+
+def part_2(data):
+    count = 0
+    i, j = start
+    blockages = set()
+    for a in range(len(locations) - 2):
+        moves = set()
+        if start != (locations[a+1][0][0], locations[a+1][0][1]):
+            if (locations[a+1][0][0], locations[a+1][0][1]) not in blockages:
+                data[locations[a+1][0][0]][locations[a+1][0][1]] = "#"
+                blockages.add((locations[a+1][0][0], locations[a+1][0][1]))
+        direction, i, j = locations[a][1], locations[a][0][0], locations[a][0][1]
+        while 0 < i < NROWS - 1 and 0 < j < NCOLS - 1:
+            direction, i, j, move = make_move(direction, i, j)
+            if move not in moves and move[0] != move[1]:
+                moves.add(move)
+            elif move[0] == move[1]:
+                continue
+            else:
+                count += 1
+                break
+        data[locations[a+1][0][0]][locations[a+1][0][1]] = "."
+    return count
+
 
 if __name__ == "__main__":
     print("======== Part 1 ========")
-    print(part_1(data))
+    count, locations = part_1(data)
+    print(count)
+
+    print("======== Part 2 ========")
+    print(part_2(data))
