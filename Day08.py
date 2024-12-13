dfc8021f0fbd0b481ed2caf3424b24c8b266846e
Giira <euan.a.marsh@gmail.com>
2024-12-13 16:26:07 +0000

    D8 Part 2

--- Day08.py
+++ Day08.py
@@ -15,6 +15,17 @@ def tuple_sub(tuple_1, tuple_2):
 def tuple_add(tuple_1, tuple_2):
     output = tuple(a+b for a,b in zip(tuple_1, tuple_2))
     return output
+
+
+def tuple_lcf(difference):
+    if difference[0] >= difference[1]:
+        big = difference[0]
+    else:
+        big = difference[1]
+    for i in range(big, -1):
+        if difference[0] % i == 0 and difference[1] % i == 0:
+            return (int(difference[0] / i), int(difference[1] / i))
+    return difference
     
 
 def get_nodes(data):
@@ -30,24 +41,37 @@ def get_nodes(data):
     return nodes
 
 
-def part_1(data):
+def solve(data):
     nodes = get_nodes(data)
     antinode_all = set()
+    antinode_extra = set()
     for node in tqdm(nodes):
         for a in range(len(nodes[node]["antennae"]) - 1):
             current = nodes[node]["antennae"][a]
+            antinode_extra.add(current)
             for node_2 in nodes[node]["antennae"][a + 1:]:
                 difference = tuple_sub(current, node_2)
+                line_vector = tuple_lcf(difference)
+                centre = current
+                while 0 <= centre[0] <= HEIGHT - 1 and 0 <= centre[1] <= WIDTH - 1:
+                    centre = tuple_sub(centre, line_vector)
+                    if 0 <= centre[0] <= HEIGHT - 1 and 0 <= centre[1] <= WIDTH - 1:
+                        antinode_extra.add(centre)
+                centre = current
+                while 0 <= centre[0] <= HEIGHT - 1 and 0 <= centre[1] <= WIDTH - 1:
+                    centre = tuple_add(centre, line_vector)
+                    if 0 <= centre[0] <= HEIGHT - 1 and 0 <= centre[1] <= WIDTH - 1:
+                        antinode_extra.add(centre)
                 antinodes = [tuple_sub(node_2, difference), tuple_add(current, difference)]
                 for antinode in antinodes:
                     if 0 <= antinode[0] <= HEIGHT - 1 and 0 <= antinode[1] <= WIDTH - 1:
                         nodes[node]["anti_node"].add(antinode)
         antinode_all = antinode_all | nodes[node]["anti_node"]
-    return len(list(antinode_all))
+    return len(list(antinode_all)), len(list(antinode_extra))
 
 if __name__ == "__main__":
     print("======== Part 1 ========")
-    print(part_1(data))
+    print(solve(data)[0])
     
     print("======== Part 2 ========")
-    # print(part_2(data))
+    print(solve(data)[1])
